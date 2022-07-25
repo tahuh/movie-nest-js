7093a13e9aacdfbefbae8df6956affe121e7c34b
tahuh <tahuh1124@gmail.com>
2022-07-25 21:35:44 +0900

    removed unused

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { IsNumber, IsString, isString } from "class-validator";
 import { CreateMovieDto } from "./create-movies.dto";
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto){}
