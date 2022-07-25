d986ca6dc509be25db525c8481fc0aadd23da6ea
tahuh <tahuh1124@gmail.com>
2022-07-25 21:26:48 +0900

    added validator

--- src/movies/dto/create-movies.dto.ts
+++ src/movies/dto/create-movies.dto.ts
@@ -1,5 +1,10 @@
+import { IsNumber, IsString, isString } from "class-validator";
+
 export class CreateMovieDto {
+    @IsString()
     readonly title: string;
+    @IsNumber()
     readonly year: number;
+    @IsString({each:true})
     readonly genres: string[];
 }
